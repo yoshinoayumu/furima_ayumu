7216dc3276fd8618e17e3d996405c8da0a1eef10
yoshinoayumu <ayumu.yoshino@di-v.co.jp>
2020-11-18 02:08:36 +0900

    単体テスト
    ①Rspecインストール
    ②--format documentation
    ③エラーメッセージの英語化
    ④userモデルファイル生成
    ⑤テストコード記載

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname { Faker::Internet.username }
+    password { '1a' + Faker::Internet.password(min_length: 7, max_length: 20) }
+    email { Faker::Internet.email }
+    # 誕生日は一意性ではないよ
+    birth_date { Faker::Date.between_except(from: 20.year.ago, to: 1.year.from_now, excepted: Date.today) }
+    first_name { '田中' }
+    last_name { '太郎' }
+    first_name_kana { 'タナカ' }
+    last_name_kana { 'タロウ' }
   end
 end
