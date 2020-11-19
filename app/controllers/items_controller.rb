653ddfa339fbc360d6c2f0ead36970394c96ce34
yoshinoayumu <ayumu.yoshino@di-v.co.jp>
2020-11-19 18:52:28 +0900

    商品編集
    ①コントローラー編集
    ②ビューファイル編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,10 +22,23 @@ class ItemsController < ApplicationController
     # アクションのnewをコールすると、エラーメッセージが入った@itemが上書きされてしまうので注意
     render 'new'
   end
-  
+
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+   return redirect_to root_path if current_user.id != @item.user.id
+ end
+
+ def update
+  @item = Item.find(params[:id])
+  @item.update(item_params) if current_user.id == @item.user.id
+  return redirect_to item_path if @item.valid?
+
+  render 'edit'
+end
 
 
   private
